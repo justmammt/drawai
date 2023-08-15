d73f4fb68e6ac2113639fe7f9abff0b0bde94a22
justmammt <justmammt69@gmail.com>
2023-08-16 00:17:31 +0200

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -30,7 +30,7 @@ const rest = new REST().setToken(token);
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
 		const data = await rest.put(
-			Routes.applicationGuildCommands(clientId),
+			Routes.applicationCommands(clientId),
 			{ body: commands },
 		);
 
